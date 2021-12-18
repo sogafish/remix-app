e39da153e2269b68ebe14b4eb10b78352610af09
sogafish <kent.soga@gmail.com>
2021-12-18 17:23:45 +0900

    fix

--- app/post.ts
+++ app/post.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import fs from 'fs/promises';
-import parserFrontMatter from 'front-matter';
+import parseFrontMatter from 'front-matter';
 import invariant from "tiny-invariant";
 
 export type Post = {
@@ -24,7 +24,7 @@ export const getPosts = async () => {
       const filePath = path.join(postsPath, fileName);
       const file = await fs.readFile(filePath);
       const stringifiedFile = file.toString();
-      const { attributes } = parserFrontMatter<PostMarkdownAttributes>(stringifiedFile);
+      const { attributes } = parseFrontMatter<PostMarkdownAttributes>(stringifiedFile);
       invariant(
         isValidPostAttributes(attributes),
         `${fileName} has bad meta data`
@@ -37,3 +37,15 @@ export const getPosts = async () => {
     }),
   );
 };
+
+export const getPost = async (slug: string) => {
+  const filepath = path.join(postsPath, slug + ".md");
+  const file = await fs.readFile(filepath);
+  const { attributes } = parseFrontMatter(file.toString());
+  invariant(
+    isValidPostAttributes(attributes),
+    `Post ${filepath} is missing attributes`
+  );
+  return { slug, title: attributes.title };
+};
+
