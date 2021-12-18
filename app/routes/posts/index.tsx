f26f7b4db6c1ce3c91bee76d8e8abcbb2fa1f66a
sogafish <kent.soga@gmail.com>
2021-12-18 15:18:27 +0900

    Add Loader

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,39 @@
+import { useLoaderData } from "remix";
+
+export const loader = () => {
+  return [
+    {
+      slug: 'first-post',
+      title: 'first post title',
+    },
+    {
+      slug: 'second-post',
+      title: 'second post title',
+    },
+    {
+      slug: 'third-post',
+      title: 'third post title',
+    },
+  ];
+};
+
+type Post = {
+  slug: string;
+  title: string;
+};
+
 const Posts = () => {
+  const posts = useLoaderData<Post[]>();
   return (
     <div>
       <h1>this is Posts</h1>
+      <ul>
+        {
+          posts.map(post => (
+            <li key={post.slug}>{post.title}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 };
