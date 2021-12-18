9e8c2bc7069829457cb0b3b1c39785673d9d0fc4
sogafish <kent.soga@gmail.com>
2021-12-18 16:18:06 +0900

    :sparkles:  Create app/posts

--- app/post.ts
+++ app/post.ts
@@ -1,23 +1,39 @@
+import path from 'path';
+import fs from 'fs/promises';
+import parserFrontMatter from 'front-matter';
+import invariant from "tiny-invariant";
+
 export type Post = {
   slug: string;
   title: string;
-}
+};
 
-export const getPosts = () => {
-  const posts: Post[] = [
-    {
-      slug: 'first-post',
-      title: 'first post title',
-    },
-    {
-      slug: 'second-post',
-      title: 'second post title',
-    },
-    {
-      slug: 'third-post',
-      title: 'third post title',
-    },
-  ];
+type PostMarkdownAttributes = {
+  title: string;
+};
 
-  return posts;
+const postsPath = path.join(__dirname, '..', 'posts');
+
+const isValidPostAttributes = (attributes: any): attributes is PostMarkdownAttributes => attributes?.title;
+
+export const getPosts = async () => {
+  const readDir = await fs.readdir(postsPath);
+
+  return Promise.all(
+    readDir.map(async fileName => {
+      const filePath = path.join(postsPath, fileName);
+      const file = await fs.readFile(filePath);
+      const stringifiedFile = file.toString();
+      const { attributes } = parserFrontMatter<PostMarkdownAttributes>(stringifiedFile);
+      invariant(
+        isValidPostAttributes(attributes),
+        `${fileName} has bad meta data`
+      );
+
+      return {
+        slug: fileName.replace(/\.md$/, ''),
+        title: attributes.title,
+      };
+    }),
+  );
 };
